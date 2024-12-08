4774908914e073847e770a80fb5686e7748e1962
Mauro Silberberg <maurosilber@gmail.com>
2024-12-08 03:55:38 -0300

    Merge tests for SmallDerivatives

--- src/scipy_events/tests/test_steady_state.py
+++ src/scipy_events/tests/test_steady_state.py
@@ -1,3 +1,4 @@
+import numpy as np
 from pytest import mark
 
 from .. import Event, SmallDerivatives, solve_ivp
@@ -5,41 +6,14 @@ from .._core import METHODS
 
 
 @mark.parametrize("method", METHODS)
-def test_small_derivatives_with_solver_tolerances(method):
-    tmax = 10
+@mark.parametrize("tol", [None, 1e-3])
+@mark.parametrize("y0", [0.0, 1.0])
+def test_small_derivatives(method, tol: float | None, y0: float):
     result = solve_ivp(
-        lambda t, y: -(y - 0.5),
-        t_span=(0, tmax),
-        y0=[1.5],
-        events=[Event(SmallDerivatives(), terminal=True)],
-        method=method,
-    )
-    assert result.t_events is not None
-    assert result.t_events[0][0] == result.t[-1]
-
-
-@mark.parametrize("method", METHODS)
-def test_small_derivatives(method):
-    tmax = 10
-    result = solve_ivp(
-        lambda t, y: -(y - 0.5),
-        t_span=(0, tmax),
-        y0=[1.5],
-        events=[Event(SmallDerivatives(atol=1e-3, rtol=1e-3), terminal=True)],
-        method=method,
-    )
-    assert result.t_events is not None
-    assert result.t_events[0][0] == result.t[-1]
-
-
-@mark.parametrize("method", METHODS)
-def test_small_derivatives_at_null_solution(method):
-    tmax = 10
-    result = solve_ivp(
-        lambda t, y: -y,
-        t_span=(0, tmax),
-        y0=[1],
-        events=[Event(SmallDerivatives(atol=1e-3, rtol=1e-3), terminal=True)],
+        lambda t, y: -(y - y0),
+        t_span=(0, np.inf),
+        y0=[y0 + 1],
+        events=[Event(SmallDerivatives(atol=tol, rtol=tol), terminal=True)],
         method=method,
     )
     assert result.t_events is not None
